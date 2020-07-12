60efa69e416214787ea17529d292d4aa19566e96
Chetan Singh <chetan.singh.0602@gmail.com>
2020-07-12 17:49:00 -0400

    Added geocoder logic for location details

--- app/src/main/java/com/example/maps_chetan_c0772216/MapsActivity.java
+++ app/src/main/java/com/example/maps_chetan_c0772216/MapsActivity.java
@@ -6,10 +6,13 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -20,17 +23,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     private static final int REQUEST_CODE = 1;
-
-    private ArrayList<String> addressResult;
+    private LatLng cityA, cityB, cityC, cityD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
         if(!hasLocationPermission()) {
             requestLocationPermission();
         }
@@ -54,8 +59,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+
         setupLocations();
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(toronto));
+
 
     }
 
@@ -63,41 +69,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void requestLocationPermission() {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
     }
-
     //    returns the boolean result weather location acess granted or not
     private boolean hasLocationPermission() {
         return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
-
+//    sets up the initial location for cities
     private void setupLocations() {
-        LatLng toronto = new LatLng(43.6532, -79.3832);
-        addLocationMarker(toronto);
-        LatLng vancouver = new LatLng(49.246292, -123.116226);
-        addLocationMarker(vancouver);
-        LatLng calgary = new LatLng(51.0447, -114.0719);
-        addLocationMarker(calgary);
-        LatLng ottawa = new LatLng(45.421532, -75.697189);
-        addLocationMarker(ottawa);
+        cityA = new LatLng(43.6532, -79.3832);
+        addLocationMarker(cityA);
+        cityB = new LatLng(49.246292, -123.116226);
+        addLocationMarker(cityB);
+        cityC = new LatLng(51.0447, -114.0719);
+        addLocationMarker(cityC);
+        cityD = new LatLng(45.421532, -75.697189);
+        addLocationMarker(cityD);
     }
-
+//  adds the location marker
     private void addLocationMarker(LatLng location) {
-        Location address = new Location("location");
-        address.setLatitude(location.latitude);
-        address.setLongitude(location.longitude);
-//        String title = addressResult.get(0);
-        String title = "";
-        String snippet = "";
+        ArrayList<String> locationDetails = getLocationAddressDetails(location);
+        String title, snippet;
+        if(locationDetails.isEmpty()) {
+            title = "Couldn't fetch details";
+            snippet = "Couldn't fetch details";
+        }
+        else {
+            title = locationDetails.get(0);
+            snippet = locationDetails.get(1);
+        }
+
         MarkerOptions locationMarker = makeMarker(location, title, snippet);
         mMap.addMarker(locationMarker);
     }
-
-
-
+//  fetches the geocoder details of latlangs
+    private ArrayList<String> getLocationAddressDetails(LatLng location) {
+        ArrayList<String> addressDetails = new ArrayList<>();
+        try {
+            Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+            List<Address> addressList = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+            Address address = addressList.get(0);
+            String title = address.getThoroughfare() + "," + address.getSubThoroughfare();
+            String snippet = address.getLocality() + "," + address.getAdminArea();
+            addressDetails.add(title);
+            addressDetails.add(snippet);
+        } catch (Exception exception) {
+            Log.i("Geolocation data fetch error", exception.getMessage());
+        }
+        return addressDetails;
+    }
+//  sets up the marker
     private MarkerOptions makeMarker(LatLng location, String title, String snippet) {
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(location);
         markerOptions.title(title);
         markerOptions.snippet(snippet);
+        markerOptions.draggable(true);
         return markerOptions;
     }
 
